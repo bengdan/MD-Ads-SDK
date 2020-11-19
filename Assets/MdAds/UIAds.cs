8ff1c105cd18b61e80137e44497c62e665fef799
Jay-Home <a1575208138@gmail.com>
2020-11-19 23:04:13 +0800

    Build Demo Scene

---
+++ Assets/MdAds/UIAds.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MdAds
+{
+    public class UIAds : MonoBehaviour
+    {
+        public InputField inputField;
+        private UniWebView _webView;
+
+        private void Start()
+        {
+            InitWebView();
+            Destroy(GetComponent<Image>());
+        }
+
+        public void LoadAd()
+        {
+            _webView.ReferenceRectTransform = GetComponent<RectTransform>();
+            _webView.Load(inputField.text);
+        }
+
+        public void ShowAd()
+        {
+            _webView.Show();
+        }
+
+        public void HideAd()
+        {
+            _webView.Hide();
+        }
+
+        private void InitWebView()
+        {
+            var webViewGameObject = new GameObject("UniWebView");
+            _webView = webViewGameObject.AddComponent<UniWebView>();
+        }
+    }
+}
