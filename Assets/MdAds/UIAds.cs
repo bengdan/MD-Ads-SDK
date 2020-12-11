b5b1fcd725963d38481888bab796f98b90a4c122
Jay <a1575208138@gmail.com>
2020-12-11 19:22:06 +0800

    Add auto show && auto hide

--- Assets/MdAds/UIAds.cs
+++ Assets/MdAds/UIAds.cs
@@ -14,11 +14,17 @@ namespace MdAds
 
         [SerializeField] 
         private bool isDebug;
+        
+        [SerializeField] 
+        private bool autoShow;
+
+        [SerializeField] 
+        private bool autoHide;
 
         private UniWebView _webView;
         private bool _noAds = true;
 
-        private void Start()
+        private void Awake()
         {
             if (TrafficInfo.Idfa == default)
             {
@@ -28,7 +34,21 @@ namespace MdAds
             Destroy(GetComponent<Image>());
         }
 
-        public void LoadAd()
+        private void OnEnable()
+        {
+            if (!autoShow) return;
+            LoadAd(true);
+        }
+
+        private void OnDisable()
+        {
+            if (autoHide)
+            {
+                HideAd();
+            }
+        }
+
+        public void LoadAd(bool showAfter = false)
         {
             var url = $"http://ads.game.melozen.com/get_ads?placementwidth={width}&placementheight={height}&os={TrafficInfo.OS}&devicemodel={TrafficInfo.DeviceModel}&idfa={TrafficInfo.Idfa}&deviceid={TrafficInfo.DeviceId}&appname={TrafficInfo.AppName}&bundle={TrafficInfo.Bundle}&appversion={TrafficInfo.AppVersion}&publisher_id=1000163&channel=MD-SDK%3A{width}x{height}";
             _webView.ReferenceRectTransform = GetComponent<RectTransform>();
@@ -43,6 +63,10 @@ namespace MdAds
             _webView.OnPageFinished += (view, code, s) =>
             {
                 _noAds = code != 200;
+                if (showAfter)
+                {
+                    ShowAd();
+                }
                 if (!isDebug) return;
                 ShowToast(code== 200 ? "Ad loaded!":$"No Ads! code :{code}");
             };
