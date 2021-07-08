45be304171d73cc53f6f97ae989e5de676d76398
Jay <a1575208138@gmail.com>
2021-07-08 15:49:03 +0800

    update

--- Assets/MdAds/UIAds.cs
+++ Assets/MdAds/UIAds.cs
@@ -8,6 +8,16 @@ namespace MdAds
     public class UIAds : MonoBehaviour
     {
         public Animator animator;
+        public Image iconImage;
+        public Image viceIconImage;
+        public Sprite defaultSprite;
+
+        private void Start()
+        {
+            StartCoroutine(UpdateSprite(iconImage, "https://play-lh.googleusercontent.com/Ar4Z09h1iNhpA7c9cGam6-QvUrTu45RYkR4ojiA0Oy5X7J1BR_1Z_a63FFCgfSoSojE"));
+            StartCoroutine(UpdateSprite(viceIconImage, "https://play-lh.googleusercontent.com/bAqioAlSPETHgkEoAk3ZumIpuBBrE7-MWi7XPqPz7CkLVwcsK4SGsa47HFYhHu2QOg"));
+            animator.Play("3 Parts");
+        }
 
         public void LoadAd(bool showAfter = false)
         {
@@ -24,10 +34,11 @@ namespace MdAds
             
         }
 
-        private static IEnumerator UpdateSprite(Image img,string mediaUrl)
+        private IEnumerator UpdateSprite(Image img,string mediaUrl)
         {   
             var request = UnityWebRequestTexture.GetTexture(mediaUrl);
             yield return request.SendWebRequest();
+            img.sprite = defaultSprite;
             if (request.result == UnityWebRequest.Result.Success)
             {
                 var tx = ((DownloadHandlerTexture)request.downloadHandler).texture;
