a6802708353a2ffb3de78e8fe9289ae4b85a5034
Jay <a1575208138@gmail.com>
2021-07-09 18:19:54 +0800

    update

--- Assets/MdAds/UIAds.cs
+++ Assets/MdAds/UIAds.cs
@@ -18,6 +18,31 @@ namespace MdAds
             StartCoroutine(UpdateSprite(iconImage, "https://play-lh.googleusercontent.com/Ar4Z09h1iNhpA7c9cGam6-QvUrTu45RYkR4ojiA0Oy5X7J1BR_1Z_a63FFCgfSoSojE"));
             StartCoroutine(UpdateSprite(viceIconImage, "https://play-lh.googleusercontent.com/bAqioAlSPETHgkEoAk3ZumIpuBBrE7-MWi7XPqPz7CkLVwcsK4SGsa47HFYhHu2QOg"));
             animator.Play("3 Parts");
+            StartCoroutine(GetRequest("http://ads.playforhigh.com/get_ads?placementwidth=512&placementheight=512&os=Android%20OS%2010%20/%20API-29%20(QKQ1.190716.003/2011091812)&devicemodel=OnePlus%20GM1910&idfa=&deviceid=0c0371bfa92a830ed319db989917be9d&appname=MD-Ads-SDK&bundle=com.MLD.MDAdsSDK&appversion=1.1.0&publisher_id=1000163&channel=MD-SDK%3A512x512&adtype=native"));
+        }
+        
+        
+        IEnumerator GetRequest(string uri)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+            {
+                // Request and wait for the desired page.
+                yield return webRequest.SendWebRequest();
+
+
+                switch (webRequest.result)
+                {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        var result = JsonUtility.FromJson<Campaign>(webRequest.downloadHandler.text);
+                        print(result);
+                        break;
+                }
+            }
         }
 
         public void LoadAd()
